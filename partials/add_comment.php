d111654bd9a4f3deff915fac0343b4508ceef1e0
Herman Byström <herman.bystrom@live.com>
2017-11-08 17:09:28 +0100

    fixed comment issue

--- partials/add_comment.php
+++ partials/add_comment.php
@@ -2,18 +2,38 @@
 session_start();
 require 'database.php';
 
-if(isset($_POST['name'], $_POST['comment'], $_POST['email'])) {
-			$Message = urlencode("Du har lagt till ett nytt inlägg.");
+if(isset($_POST['name'], $_POST['comment'], $_POST['email'], $_SESSION["user"]["userID"], $_SESSION["user"]["username"])) {
 			header("Location: ../index.php?postID=".$_POST['postID']);
 
 			$statement = $pdo->prepare(
-			"INSERT INTO comments (postID, content, name, time, email) 
-			VALUES (:postID, :content, :name, :time, :email) "
+			"INSERT INTO comments (userID, postID, content, username, name, time, email) 
+			VALUES (:userID, :postID, :content, :username, :name, :time, :email) "
 			);
 			$date = date('Y-m-d H:i:s');
 			$statement->execute(array(
+			":userID"			 => $_SESSION["user"]["userID"],
 			":postID"      => $_POST['postID'],
 			":content"     => $_POST['comment'],
+			":username"    => $_SESSION["user"]["username"],
+			":name"     	 => $_POST['name'],
+			":time"     	 => $date,
+			":email"     	 => $_POST['email']
+			
+			));
+}
+elseif(isset($_POST['name'], $_POST['comment'], $_POST['email'])) {
+			header("Location: ../index.php?postID=".$_POST['postID']);
+
+			$statement = $pdo->prepare(
+			"INSERT INTO comments (userID, postID, content, username, name, time, email) 
+			VALUES (:userID, :postID, :content, :username, :name, :time, :email) "
+			);
+			$date = date('Y-m-d H:i:s');
+			$statement->execute(array(
+			":userID"			 => "0",
+			":postID"      => $_POST['postID'],
+			":content"     => $_POST['comment'],
+			":username"    => "0",
 			":name"     	 => $_POST['name'],
 			":time"     	 => $date,
 			":email"     	 => $_POST['email']
