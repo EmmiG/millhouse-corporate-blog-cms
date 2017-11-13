626103aa4d01867b703cc696bfbdac84654e1fc5
Herman Byström <herman.bystrom@live.com>
2017-11-13 12:45:30 +0100

    design for register

--- edit_post.php
+++ edit_post.php
@@ -9,51 +9,50 @@
 				":postID"     => $_POST["postID"]
 				));
 
-			$indivudual_post = $statement->fetchAll(PDO::FETCH_ASSOC);
+			$indivudual_post = $statement->fetchAll(PDO::FETCH_ASSOC); ?>
+            
+            <div id="blog" class="content_wrap">
+<?php
+                foreach($indivudual_post as $post) { ?>
+                    <div class="entry_box">
+                        <h4>Ändra inlägg</h4>
+                        <form action="partials/edit_entry.php" method="POST">
+                            <div class="comment_field col-sm-12">
+                                <div class="form-group">
+                                    <label for="title"> Titel </label>
+                                    <input type="text" name="title" class="form-control" value="<?= $post["title"]; ?>" required>
+                                </div>
+                            </div>
 
-			foreach($indivudual_post as $post) { ?>
-				<div class="container mt-5">
-					<h4>Ändra inlägg</h4>
-					<form action="partials/edit_entry.php" method="POST">
-						<div class="form-group">
+                            <div class="form-group">
+                                <label for="content"> Innehåll </label>
+                                <input type="text" name="content" class="form-control" value="<?= $post["content"]; ?>" required>
 
-							<label for="title"> Titel </label>
+                            </div>
+                            <div class="form-group">
+                                <label for="category"> Kategori </label><br>
+                                <select class="custom-select" name="category" required>
+                                    <option selected><?= $post["category"]; ?></option>
+                                    <option value="Frukter">Frukter</option>
+                                    <option value="Verktyg">Verktyg</option>
+                                </select>
 
-							<input type="text" name="title" class="form-control" value="<?= $post["title"]; ?>" required>
-						</div>
-						<div class="form-group">
-							<label for="content"> Innehåll </label>
-
-							<input type="text" name="content" class="form-control" value="<?= $post["content"]; ?>" required>
-
-						</div>
-						<div class="form-group">
-							<label for="category"> Kategori </label><br>
-
-							<select class="custom-select" name="category" required>
-								<option selected><?= $post["category"]; ?></option>
-								<option value="Frukter">Frukter</option>
-								<option value="Verktyg">Verktyg</option>
-							</select>
-
-						</div>
-						<div class="form-group">
-							<label for="comment"> E-mail </label>
-
-							<input type="email" name="email" class="form-control" value="<?= $post["email"]; ?>" required>
-
-						</div>
-						<div class="form-group">
-							<input type="hidden" value="<?=$_POST["postID"]; ?>" name="postID">
-							<input type="submit" class="btn btn-primary">
-						</div>
-					</form>
-				</div>
-			<?php	
+                            </div>
+                            <div class="form-group">
+                                <label for="comment"> E-mail </label>
+                                <input type="email" name="email" class="form-control" value="<?= $post["email"]; ?>" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="hidden" value="<?=$_POST["postID"]; ?>" name="postID">
+                                <input type="submit" class="btn btn-primary">
+                            </div>
+                        </form>
+                    </div>
+                </div>				
+			<?php
 				} 
 				if(isset($_SERVER['HTTP_REFERER'])) {
 					echo "<a href=".$_SERVER['HTTP_REFERER'].">Gå tillbaka</a>";
 					}
 				}
 			?>
-			
--- register_user.php
+++ register_user.php
@@ -1,29 +1,35 @@
 <?php	require 'partials/head.php'; ?>
- <div class="container mt-5">
-  <h4>Register</h4>
-  <form action="partials/register.php" method="POST">
-    <div class="form-group">
 
-      <label for="username"> Username </label>
-
-      <input type="text" name="username" class="form-control">
-    </div>
-    <div class="form-group">
-      <label for="password"> Password </label>
-
-      <input type="password" name="password" class="form-control">
-
-    </div>
-    <div class="form-group">
-
-      <input type="submit" class="btn btn-primary">
-    </div>
-  </form>
-  
-</div>
+<div class="jumbotron jumbo_login"> 
+    <div class="container">
+        <div class="row">
+            <div class="login_box">
+                <img src="images/millhouse_white_logo.svg">
+                <h3>Register</h3>
+                    <form action="partials/register.php" method="POST">
+                    <div class="form-group">
+                        <label for="username"> Username </label>
+                        <input type="text" name="username" class="form-control" placeholder="Username">
+                    </div>
+                    <div class="form-group">
+                        <label for="password"> Password </label>
+                        <input type="password" name="password" class="form-control" placeholder="***********">
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-primary">
+                    </div>
+                    <div class="register_text">
+                        <p>Already have an account?</p>
+                        <p><a href="landing.php">Log in here!</a></p>
+                    </div>
+                  </form>
+                </div>
+            </div>
+        </div> 
+    </div> 
+    
 <?php
 	if(isset($_SERVER['HTTP_REFERER'])) {
-		echo "<a href=".$_SERVER['HTTP_REFERER'].">Gå tillbaka</a>";
+		echo "<a href=".$_SERVER['HTTP_REFERER'].">Go back</a>";
 				}
 require 'partials/footer.php'; ?>
-
