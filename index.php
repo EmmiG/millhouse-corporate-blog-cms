2f9c9fff5ecd2eb920d0bcc5f75ac0f1af51053a
Herman Byström <herman.bystrom@live.com>
2017-11-25 16:23:55 +0100

    style pagination

--- index.php
+++ index.php
@@ -73,7 +73,7 @@
 						$total_pages = ceil($total_records / $limit);  
 						$page_links = '<nav aria-label="Page navigation example"><ul class="pagination">';
 						if(isset($_GET['page']) && $_GET['page'] > 1) {
-							$page_links .= "<li class='page-item'><a class='page-link' href='index.php?page=".($_GET['page'] - 1)."' aria-label='Previous'><span aria-hidden='true'>&laquo;</span><span class='sr-only'>Previous</span></a></li>";
+							$page_links .= "<li class='page-item'><a class='page-link' href='index.php?page=".($_GET['page'] - 1)."' aria-label='Previous'><span aria-hidden='true' class='glyphicon glyphicon-chevron-left'><span class='sr-only'>Previous</span></a></li>";
 						}
 						if(empty($_GET['page'])) {
 							for ($i=1; $i<=$total_pages; $i++) {  
@@ -85,7 +85,7 @@
 								 }
 							};   
 							if($total_pages > 1) {
-								$page_links .= "<li class='page-item'><a class='page-link' href='index.php?page=2' aria-label='Next'><span aria-hidden='true'>&raquo;</span><span class='sr-only'>Next</span></a></li>";
+								$page_links .= "<li class='page-item'><a class='page-link' href='index.php?page=2' aria-label='Next'><span aria-hidden='true' class='glyphicon glyphicon-chevron-right'><span class='sr-only'>Next</span></a></li>";
 							}
 							echo $page_links . "</ul></nav>"; 
 						}
@@ -99,7 +99,7 @@
 								 }
 							}
 							if($_GET['page'] < $total_pages) {
-								$page_links .= "<li><a class='page-link' href='index.php?page=".($_GET['page'] + 1)."' aria-label='Next'><span aria-hidden='true'>&raquo;</span><span class='sr-only'>Next</span></a></li>";
+								$page_links .= "<li><a class='page-link' href='index.php?page=".($_GET['page'] + 1)."' aria-label='Next'><span aria-hidden='true' class='glyphicon glyphicon-chevron-right'></span><span class='sr-only'>Next</span></a></li>";
 								
 							}
 							echo $page_links . "</ul></nav>";  
