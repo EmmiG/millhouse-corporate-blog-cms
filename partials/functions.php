3b3e4253e28b0671cfa7f0d7e08f1ab2290af217
Herman Byström <herman.bystrom@live.com>
2017-11-28 13:53:29 +0100

    improved file structure

--- partials/functions.php
+++ partials/functions.php
@@ -49,5 +49,21 @@ function count_user_comments() {
 	}
 }
 
+function duplet() {
+	require 'database.php';
+	$statement = $pdo->prepare("SELECT COUNT(username) AS num FROM users WHERE username = :username");
+	$statement->execute(array(
+	":username"     => $_POST["username"]
+	));
+	$duplet = $statement->fetch(PDO::FETCH_ASSOC);
+	
+	 if($duplet['num'] > 0){
+			return true;
+	}
+	else {
+			return false;
+	}
+}
+
 
 ?>
