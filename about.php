426c37cc40ebf4a12de6adc8b4d97f4d30e1d135
Dragoness390 <cera11@hotmail.se>
2017-11-29 12:41:54 +0100

    fixed footer

---
+++ about.php
@@ -0,0 +1,35 @@
+<?php
+    session_start();
+    require 'partials/head.php';
+?>
+
+
+<div class="main_wrap">
+
+    <div class="about_wrap content_wrap">
+       
+        <div class="about_row row">
+           <h1>Lorem ipsum dolor sit</h1>
+            <div class="about_text col-sm-6">
+                <article>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam luctus at tellus sit amet condimentum. Proin elit ipsum, volutpat non porttitor vitae, consectetur eget nisi. Sed at volutpat odio. In consequat ac turpis ut vulputate. Ut feugiat neque quis vehicula cursus. Donec a augue justo. Donec quis porttitor neque, eu vulputate purus.
+                    Mauris porta mattis erat sit amet rutrum. Quisque mattis purus eu scelerisque ornare. Suspendisse id velit rutrum, tincidunt nisl sit amet, porttitor mi. Pellentesque pretium libero sed nulla mollis iaculis. Nam sit amet risus velit. Donec fermentum ornare purus, sit amet consectetur magna dapibus faucibus. Vestibulum ut gravida nisl. Proin ut lacus aliquam, pulvinar tortor sed, interdum ipsum. Sed lobortis nunc eget dignissim ornare. Pellentesque at congue risus. Donec risus ipsum, vulputate non pulvinar sit amet, placerat vitae urna. In a ex sit amet dui ultrices tempus. Suspendisse eget lacinia felis, eget vestibulum risus. Vivamus libero sapien, pellentesque vitae cursus eu, vestibulum aliquam leo. Duis ornare vel ante id gravida.
+                    </p>
+                </article>
+            </div><!-- End of about_text col div-->
+            
+              <div class="about_image col-sm-6">
+                <img src="Images/about@1440px.jpg" alt="about_image">
+                
+            </div><!-- End of image col div-->
+            
+        </div><!-- End of about_row row div-->
+    </div> <!-- End of about_wrap content_wrap div-->
+    
+    
+</div><!-- End main_wrap div-->
+
+<?php 
+require 'partials/footer.php';
+?>
+
