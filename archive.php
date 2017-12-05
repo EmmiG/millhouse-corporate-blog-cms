a4080e8ef80cc2bf51f7cd2a996130bae154d246
Vanja Ivanovic <vanja.i@hotmail.com>
2017-12-05 15:52:21 +0100

    archive3

--- archive.php
+++ archive.php
@@ -1,39 +1,40 @@
 <?php 
-    require_once 'partials/session_start.php';
+    session_start(); 
     require 'partials/head.php';
     require 'partials/fetch_months.php';
-?>
+    require 'partials/functions.php';
+    ?>
 
-<div class="main_wrap">
+<div id="content_archive" class="main_wrap">
     <div class="container">
         <div id="archive" class="content_wrap">
             <div class="row">
-                <div class="col-sm-12">
-                    <div class="card_header">
-                        <h3>View all posts</h3>
-                    </div>
-                    <div class="card_content">
-                    <?php 
-                    //If the GET-request is set a foreach loop will iterate through the SQL-request.
-                    if(isset($_GET['month'])) {
-                        require 'partials/sort_months.php';
-                        foreach($selected_posts as $selpost) { ?>
-                        <div class="recent_loop row">
-                            <div class="col-sm-9">
-                                <h4><?= $selpost['title'] ?></h4>
-                                <h5><?= $selpost['time'] ?></h5>
-                                <p><?= $selpost['content'] ?></p>
-                                <p><?= $selpost['email'] ?></p>
+                <div class="col-sm-12 posts_wrap">
+                    <h1><?=$_GET['month']?></h1>
+                    <div id="blog" class="card_content">
+                        <article>
+                            <?php 
+
+                            if(isset($_GET['month'])) {
+                                require 'partials/sort_months.php';
+                                foreach($selected_posts as $selpost) { ?>
+                            <div class="archive_box">
+                                <a href="comment.php?postID=<?= $selpost['postID'] ?>">
+                                <h4 class="archive_title"><?= $selpost['title'] ?></h4></a>
+                                <h5><?= $selpost['time']?> | <?= $selpost['name'] ?></h5>
+                                <!--<article><?= $selpost['content'] ?></article>-->
+                                <!--<p><?= $selpost['email'] ?></p>-->
                             </div>
-                   
-                        </div>  
-                     <?php }} ?>
-                    </div>
-                </div>
-            </div>
+                                 
+                             <?php }} ?>
+                        </article>
+                     </div><!--card_content-->
+                </div><!--stänger col-sm-12-->
+            </div><!--stänger row-->
         </div><!--stänger content_wrap-->
     </div><!--stänger container-->
 </div><!--stänger main_wrap-->
+
 
 <?php  require 'partials/footer.php'; ?>
 
