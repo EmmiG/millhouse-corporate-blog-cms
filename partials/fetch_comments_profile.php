40dce67ba5eec6f9cf3210aaa9993aef2d1900a0
Herman Byström <herman.bystrom@live.com>
2017-12-05 12:33:01 +0100

    fixed pagination bug

--- partials/fetch_comments_profile.php
+++ partials/fetch_comments_profile.php
@@ -6,7 +6,7 @@ if(isset($_GET["page"])) {
 	$page=1; 
 };  
 $start_from = ($page-1) * $limit; 
-$statement = $pdo->prepare("SELECT * FROM comments order by ID DESC LIMIT 5");
+$statement = $pdo->prepare("SELECT * FROM comments order by ID DESC LIMIT $start_from, $limit");
 $statement->execute(); 
 $count = $statement->fetchAll(PDO::FETCH_ASSOC);
 ?>
