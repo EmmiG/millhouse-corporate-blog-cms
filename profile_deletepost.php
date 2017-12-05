2badb400afd22856fed09243b117e2ed2554df48
Herman Byström <herman.bystrom@live.com>
2017-12-05 16:30:14 +0100

    first add

--- profile_deletepost.php
+++ profile_deletepost.php
@@ -1,9 +1,10 @@
 <?php
     require_once 'partials/session_start.php';
+    //This page will only appear if the user is logged in. If not logged in, he/she will be re-directed.
     if(isset($_SESSION['user'])) {
-    require 'partials/database.php';
-    require 'partials/head_profile.php';
-    require 'partials/functions.php';
+        require 'partials/database.php';
+        require 'partials/head_profile.php';
+        require 'partials/functions.php';
 ?>
 <div id="content" class="container">
     <div class="row">
@@ -12,29 +13,31 @@
                 <h3>Which post would you like to delete?</h3>    
             </div>
             <div class="card_content">
+            <!-- A partial will run a SQL-request to fetch the entries made by a particular user. Then they are looped out thorugh a foreach loop. The user who made the entries can delete them as they will not appear otherwise. -->
             <?php 
-						require 'partials/fetch_individual_entries_profile.php';
-            foreach($indivudual_post_profile as $post) { ?>
-                <div class="recent_loop row">
-                    <div class="col-sm-9">
-                        <h4><?= $post['title'] ?></h4>
-                        <h5><?= $post['time'] ?></h5>
-                        <p><?= $post['content'] ?></p>
-                        <!--<p> <?= $c['email'] ?></p>-->
-                    </div>
-                    <div class="col-sm-3">
-                        <?php if(isset($_SESSION["user"]["username"])) {?>      
-                        <form action="partials/delete_entry.php" method="post">
-                            <input type="hidden" value="<?=$post["postID"] ?>" name="postID"/>
-                            <input type="submit" value="delete" class="btn btn-primary btn_card"/>
-                        </form>
-                    </div> 
-                </div>  
+                require 'partials/fetch_individual_entries_profile.php';
+                foreach($indivudual_post_profile as $post) { 
+            ?>
+            <div class="recent_loop row">
+                <div class="col-sm-9">
+                    <h4><?= $post['title'] ?></h4>
+                    <h5><?= $post['time'] ?></h5>
+                    <p><?= $post['content'] ?></p>
+                    <!--<p> <?= $c['email'] ?></p>-->
+                </div>
+                <div class="col-sm-3">     
+                    <form action="partials/delete_entry.php" method="post">
+                        <input type="hidden" value="<?=$post["postID"] ?>" name="postID"/>
+                        <input type="submit" value="delete" class="btn btn-primary btn_card"/>
+                    </form>
+                </div> 
+            </div>  
  
-            <?php }} ?>
+            <?php } ?>
             </div>
         </div>
     </div>
+    <!--  A function is initiated to gather the post amount. Then our pagination is required. -->
     <?php
         $total_records = postamount_individual();
         require 'partials/pagination_pages.php';
