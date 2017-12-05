681dc87ab0ada79e6929a5a2fa99a7bcef211cf3
Vanja Ivanovic <vanja.i@hotmail.com>
2017-12-05 15:11:08 +0100

    sista archive.php

--- archive.php
+++ archive.php
@@ -2,41 +2,48 @@
     session_start(); 
     require 'partials/head.php';
     require 'partials/fetch_months.php';
-?>
+    require 'partials/functions.php';
+    ?>
 
 <div id="content_archive" class="main_wrap">
     <div class="container">
         <div id="archive" class="content_wrap">
             <div class="row">
                 <div class="col-sm-12 posts_wrap">
-                    <div class="card_header">
-                        <h3><?=$_GET['month']?></h3>
-                    </div>
-                    <div id="blog" class="card_content">
+                    <h1><?=$_GET['month']?></h1>
+                <div id="blog" class="card_content">
                     <article>
-                    <?php 
+                        <?php 
 
-                    if(isset($_GET['month'])) {
-                        require 'partials/sort_months.php';
-                        foreach($selected_posts as $selpost) { ?>
+                        if(isset($_GET['month'])) {
+                            require 'partials/sort_months.php';
+                            foreach($selected_posts as $selpost) { ?>
                         <div class="archive_box">
-                          
-                                <h4><?= $selpost['title'] ?></h4>
-                                <h5><?= $selpost['time'] ?></h5>
-                                <article><?= $selpost['content'] ?></article>
-                                <p><?= $selpost['email'] ?></p>
-                            </div>
-                   
-                         
-                     <?php }} ?>
-                      </article>
-                    </div>
+                        <a href="comment.php?postID=<?= $selpost['postID'] ?>">
+                        <h4 class="archive_title"><?= $selpost['title'] ?></h4></a>
+                        <h5><?= $selpost['time']?> | <?= $selpost['name'] ?></h5>
+                        <!--<article><?= $selpost['content'] ?></article>-->
+                        <!--<p><?= $selpost['email'] ?></p>-->
+                        </div>
+                             
+                         <?php }} ?>
+                    </article>
+                 </div>
                 </div>
-
             </div>
 
         </div><!--stänger content_wrap-->
     </div><!--stänger container-->
+
+        <div class="pagination">  
+            <?php
+            $total_records = postamount();
+            require 'partials/pagination_pages.php';
+            ?> 
+        </div>
+    <div class="clear"></div>  
+</div>
+
 </div><!--stänger main_wrap-->
 
 <?php  require 'partials/footer.php'; ?>
--- partials/sort_months.php
+++ partials/sort_months.php
@@ -1,11 +1,21 @@
 <?php
-	require 'database.php';
+	require 'partials/database.php';
+
+	$limit = 5;  
+if(isset($_GET["page"])) {
+	$page  = $_GET["page"]; 
+} else { 
+	$page=1; 
+};  
+$start_from = ($page-1) * $limit;  
 
 	$selected_month = date("m", strtotime($_GET['month']));
 
-	$statement = $pdo->prepare("SELECT * FROM posts WHERE MONTH(time) = :month order by postID DESC"); 
+	$statement = $pdo->prepare("SELECT * FROM posts WHERE MONTH(time) = :month order by postID DESC LIMIT $start_from, $limit"); 
 	$statement->execute(array(
 				":month"     => $selected_month
 				));
 	$selected_posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
 	?>
